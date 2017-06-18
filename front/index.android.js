ec3cae763e5a81a94b487e240fe39b4015da0f38
fa93hws <wjun0912@gmail.com>
2017-06-19 02:17:57 +1000

    Design of login page

--- front/index.android.js
+++ front/index.android.js
@@ -8,24 +8,62 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
+  Button,
+  Dimensions,
+  TextInput,
   View
 } from 'react-native';
 
 export default class front extends Component {
   render() {
+    let width = Dimensions.get('window').width;
+    width = width * 0.9;
+    let height = Dimensions.get('window').height;
+    let email = '\u2709';
+    let pass = '\uD83D\uDD12';
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.footHeader}/>
+        <Image
+          source={require('./logo.png')}
+          style={{flex:1,height:100,width:300,resizeMode: 'contain'}}
+        />
+        <View
+          style={{flex:4,width:width}}
+        >
+          <View style={{flex:1}}></View>
+          <View style={{flex:2}}>
+            <TextInput style = {styles.inputBox} placeholder="Email/User name" placeholderTextColor="grey">
+            </TextInput>
+          </View>
+          <View style={{flex:2}}>
+            <TextInput style = {styles.inputBox} placeholder="Password" placeholderTextColor="grey">
+            </TextInput>
+          </View>
+        </View>
+        <View
+          style={{flex:3,width:width}}
+        >
+          <Button
+            title="Login"
+            color='#393e46'
+          />
+          <View style={{flexDirection:'row'}}>
+            <Text
+              style={{paddingTop:30,color:'white'}}
+            >Don't have an account?</Text>
+            <Text
+              style={{paddingTop:30,color:'white',fontWeight:'bold',textDecorationLine:'underline'}}
+            >Sign Up</Text>
+          </View>
+        </View>
+        <View
+          style={{flex:1}}
+        >
+
+        </View>
       </View>
     );
   }
@@ -34,20 +72,21 @@ export default class front extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#222831',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  footHeader:{
+    flex:1
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  icon:{
+    fontSize:30,
+    color:'white'
   },
+  inputBox:{
+    fontSize:20,
+    color:'white'
+  }
+
 });
 
 AppRegistry.registerComponent('front', () => front);
